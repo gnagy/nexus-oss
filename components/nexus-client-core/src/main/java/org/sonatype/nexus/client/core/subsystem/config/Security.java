8e03c6f9f22ef8b05d3ab03c8a8d90a7296f5f01
Alin Dreghiciu <adreghiciu@gmail.com>
2013-08-10 01:34:31 -0700

    Merge pull request #88 from sonatype/security-settings
    Security settings nexus client
    Conflicts:
    	components/nexus-client-core/src/main/java/org/sonatype/nexus/client/internal/rest/jersey/subsystem/JerseyServerConfiguration.java

--- components/nexus-client-core/src/main/java/org/sonatype/nexus/client/core/subsystem/config/Security.java
+++ components/nexus-client-core/src/main/java/org/sonatype/nexus/client/core/subsystem/config/Security.java
@@ -10,37 +10,16 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-package org.sonatype.nexus.client.core.subsystem;
 
-import org.sonatype.nexus.client.core.subsystem.config.RemoteProxy;
-import org.sonatype.nexus.client.core.subsystem.config.RestApi;
-import org.sonatype.nexus.client.core.subsystem.config.Security;
+package org.sonatype.nexus.client.core.subsystem.config;
 
 /**
- * Server configuration subsystem.
+ * Security configuration segment.
  *
- * @since 2.2
+ * @since 2.7
  */
-public interface ServerConfiguration
+public interface Security
+    extends Segment<Security, SecuritySettings>
 {
-
-    /**
-     * @return Remote Proxy configuration segment.
-     * @since 2.6
-     */
-    RemoteProxy remoteProxySettings();
-
-    /**
-     * @return Rest API configuration segment.
-     * @since 2.6.1
-     */
-    RestApi restApi();
-
-  /**
-   * Returns {@link Security} configuration segment.
-   *
-   * @since 2.7
-   */
-  Security security();
 
 }
--- components/nexus-client-core/src/main/java/org/sonatype/nexus/client/internal/rest/jersey/subsystem/JerseyServerConfiguration.java
+++ components/nexus-client-core/src/main/java/org/sonatype/nexus/client/internal/rest/jersey/subsystem/JerseyServerConfiguration.java
@@ -10,14 +10,17 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
+
 package org.sonatype.nexus.client.internal.rest.jersey.subsystem;
 
 import org.sonatype.nexus.client.core.spi.SubsystemSupport;
 import org.sonatype.nexus.client.core.subsystem.ServerConfiguration;
 import org.sonatype.nexus.client.core.subsystem.config.RemoteProxy;
 import org.sonatype.nexus.client.core.subsystem.config.RestApi;
+import org.sonatype.nexus.client.core.subsystem.config.Security;
 import org.sonatype.nexus.client.internal.rest.jersey.subsystem.config.JerseyRemoteProxy;
 import org.sonatype.nexus.client.internal.rest.jersey.subsystem.config.JerseyRestApi;
+import org.sonatype.nexus.client.internal.rest.jersey.subsystem.config.JerseySecurity;
 import org.sonatype.nexus.client.rest.jersey.JerseyNexusClient;
 
 /**
@@ -28,47 +31,59 @@ public class JerseyServerConfiguration
     implements ServerConfiguration
 {
 
-    /**
-     * Http Proxy configuration segment.
-     * Lazy initialized on first request.
-     */
-    private RemoteProxy remoteProxy;
+  /**
+   * Http Proxy configuration segment.
+   * Lazy initialized on first request.
+   */
+  private RemoteProxy remoteProxy;
 
-    /**
-     * Rest API configuration segment.
-     * Lazy initialized on first request.
-     */
-    private RestApi restApi;
+  /**
+   * Rest API configuration segment.
+   * Lazy initialized on first request.
+   */
+  private RestApi restApi;
 
-    public JerseyServerConfiguration( final JerseyNexusClient nexusClient )
-    {
-        super( nexusClient );
+  /**
+   * {@link Security} configuration segment.
+   * Lazy initialized on first request.
+   */
+  private Security security;
+
+  public JerseyServerConfiguration(final JerseyNexusClient nexusClient) {
+    super(nexusClient);
+  }
+
+  /**
+   * @since 2.6
+   */
+  @Override
+  public RemoteProxy remoteProxySettings() {
+    if (remoteProxy == null) {
+      remoteProxy = new JerseyRemoteProxy(getNexusClient());
     }
+    return remoteProxy;
+  }
 
-    /**
-     * @since 2.6
-     */
-    @Override
-    public RemoteProxy remoteProxySettings()
-    {
-        if ( remoteProxy == null )
-        {
-            remoteProxy = new JerseyRemoteProxy( getNexusClient() );
-        }
-        return remoteProxy;
+  /**
+   * @since 2.6.1
+   */
+  @Override
+  public RestApi restApi() {
+    if (restApi == null) {
+      restApi = new JerseyRestApi(getNexusClient());
     }
+    return restApi;
+  }
 
-    /**
-     * @since 2.6.1
-     */
-    @Override
-    public RestApi restApi()
-    {
-        if ( restApi == null )
-        {
-            restApi = new JerseyRestApi( getNexusClient() );
-        }
-        return restApi;
+  /**
+   * @since 2.7
+   */
+  @Override
+  public Security security() {
+    if (security == null) {
+      security = new JerseySecurity(getNexusClient());
     }
+    return security;
+  }
 
 }
